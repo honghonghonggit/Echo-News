ba74b5ea3791d56a0c06fa6ac802afb93c46a553
goworkufokhv <86183582+goworkufokhv@users.noreply.github.com>
2026-05-10 20:27:31 +0900

    can't find BigAutoField, even models directory
    Clarified the purpose of default_auto_field in NewsConfig.

--- news/apps.py
+++ news/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField' #models에 BigAutoField에 있는 데이터를 대입한다고 했는데 정작 github에는 models 디렉터리가 없는데?
     name = 'news'
