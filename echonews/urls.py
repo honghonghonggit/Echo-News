e33aba0970d876cd6aeb2d70a183804f7cece6b7
goworkufokhv <86183582+goworkufokhv@users.noreply.github.com>
2026-05-17 20:52:47 +0900

    urls.py explanation

--- echonews/urls.py
+++ echonews/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('news.urls')),
+    path('admin/', admin.site.urls), # 관리자(Admin) 페이지 연결
+    path('', include('news.urls')), # 사용자용 뉴스 앱 URL 연결
 ]
