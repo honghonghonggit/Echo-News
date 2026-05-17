6ba4b23bcfe92939131b3cc953e519de88ff38db
goworkufokhv <86183582+goworkufokhv@users.noreply.github.com>
2026-05-17 21:02:34 +0900

    apps.py comments

--- news/apps.py
+++ news/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class NewsConfig(AppConfig):
     """news 앱의 기본 설정 클래스"""
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news'
+    name = 'news' #Django가 이 앱을 인식할 때 'news'라는 이름으로 등록합니다.
+
