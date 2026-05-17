b7cc0cbe9af90aab3251d352bda1f10b3e1566ea
goworkufokhv <86183582+goworkufokhv@users.noreply.github.com>
2026-05-17 20:56:13 +0900

    wsgi.py explanations

--- echonews/wsgi.py
+++ echonews/wsgi.py
@@ -4,6 +4,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'echonews.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'echonews.settings') #Django가 사용할 설정 파일(settings.py)을 환경 변수로 지정합니다.
 
-application = get_wsgi_application()
+application = get_wsgi_application() #웹 서버가 요청을 받으면 이 application을 통해 Django 내부로 전달됩니다.
